2f232238e135c00f0baf1aace1b1d1f2e3e8e817
Leonardo-Rozza <leonardo155964@gmail.com>
2023-08-08 10:14:20 -0300

    usando el useState con la api

--- src/03-Examples/MultipleCustomHooks.jsx
+++ src/03-Examples/MultipleCustomHooks.jsx
@@ -1,10 +1,34 @@
+import { useCounter } from "../hooks/UseCounter";
 import { useFetch } from "../hooks/useFetch";
 
 export const MultipleCustomHooks = () => {
-  const {} = useFetch("https://rickandmortyapi.com/api/character/2");
+  const { counter, increment } = useCounter(1);
+
+  const { data, isLoading } = useFetch(
+    `https://rickandmortyapi.com/api/character/${counter}`
+  );
+  const { name, species } = !!data && data;
+
   return (
     <>
       <h1>BreakingBad Quotes</h1>
+      <hr />
+      {isLoading ? (
+        <div className="alert alert-info text-center">Cargando...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{species}</p>
+          <footer className="blockquote-footer">{name}</footer>
+        </blockquote>
+      )}
+
+      <button
+        className="btn btn-secondary"
+        disabled={isLoading}
+        onClick={() => increment()}
+      >
+        Siguiente personaje
+      </button>
     </>
   );
 };
